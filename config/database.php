2b235d430f0fa12417aeb2fa727bf5d56bde784e
petidesoftwares <petideenterprise@gmail.com>
2021-09-14 15:26:57 +0100

    db update

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL=parse_url('mysql://b2e6276d149dbe:4d417d28@us-cdbr-east-04.cleardb.com/heroku_400217bbfecf1ed?reconnect=true');
+//$DATABASE_URL=parse_url('mysql://b2e6276d149dbe:4d417d28@us-cdbr-east-04.cleardb.com/heroku_400217bbfecf1ed?reconnect=true');
 
 return [
 
@@ -48,16 +48,16 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-//            'host' => env('DB_HOST', '127.0.0.1'),
-        'host'=>$DATABASE_URL['host'],
-            'port'=>$DATABASE_URL['port'],
-            'database'=>ltrim($DATABASE_URL["path"], "/"),
-            'username'=>$DATABASE_URL['user'],
-            'password'=>$DATABASE_URL['password'],
-//            'port' => env('DB_PORT', '3306'),
-//            'database' => env('DB_DATABASE', 'forge'),
-//            'username' => env('DB_USERNAME', 'forge'),
-//            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', '127.0.0.1'),
+//        'host'=>$DATABASE_URL['host'],
+//            'port'=>$DATABASE_URL['port'],
+//            'database'=>ltrim($DATABASE_URL["path"], "/"),
+//            'username'=>$DATABASE_URL['user'],
+//            'password'=>$DATABASE_URL['password'],
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
